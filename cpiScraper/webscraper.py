5f6a6b6d90686efee6d6453bdd20f68a7b601e95
allesman <allesman.stachelx@gmail.com>
2023-07-26 11:20:50 +0200

    getting fatf through web scraping and not selenium

--- cpiScraper/webscraper.py
+++ cpiScraper/webscraper.py
@@ -4,33 +4,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import requests
+from lxml import html
 
-def waitTillLoaded(driver):
-    WebDriverWait(driver=driver, timeout=10).until(
-        lambda x: x.execute_script("return document.readyState === 'complete'")
-    )
-def openWebsite():
-    # open the browser
-    options= webdriver.ChromeOptions()
-    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    # open the website
-    driver.get("https://www.knowyourcountry.com/fatf-aml-list/")
-    # wait for the page to load
-    waitTillLoaded(driver)
-    return driver
-def getList(black,driver):
+# headers, because the website doesn't allow scraping without a user agent
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+
+def getList(black):
     # gets the fatf grey or black list
-    # get the text from the following xpath when its loaded: /html/body/div[2]/main/div[3]/div/section/div/div/div/div/div/p[4]
-    # element = WebDriverWait(driver, 10).until()
-    element = driver.find_element(By.XPATH, f"/html/body/div[2]/main/div[3]/div/section/div/div/div/div/div/p[{4 if black else 5}]")
+
+    # open the website
+    page=requests.get("https://www.knowyourcountry.com/fatf-aml-list/",headers=headers)
+    # get the html tree
+    tree = html.fromstring(page.content)
     # get the text
-    if black:
-        text = element.text.split("\n")[1:]
-    else:
-        text = element.text.split("\n")[2:]
-    # remove the first three characters from each line
+    text = tree.xpath(f"/html/body/div[1]/main/div[3]/div/section/div/div/div/div/div/p[{4 if black else 5}]/text()")
+    # remove the first line, as it's just info text and not a country
+    text=text[1:]
+    # remove the first three characters from each line, only being enumeration (e.g. "1. ")
     text = [line[3:] for line in text]
     # remove any space character at the beginning or end of each line
     text = [line.strip() for line in text]
@@ -39,10 +32,33 @@ def getList(black,driver):
 
 def getBothLists():
     # returns a list of two lists, the first being the fatf black list and the second being the fatf grey list
-    driver = openWebsite()
     output = []
-    output.append(getList(True,driver))
-    output.append(getList(False,driver))
+    output.append(getList(True))
+    output.append(getList(False))
     return output
 
-# print(getBothLists())
+print(getBothLists())
+
+# old code, using selenium:
+
+# def openWebsite():
+#     # open the browser
+#     options= webdriver.ChromeOptions()
+#     options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+#     service = Service(ChromeDriverManager().install())
+#     driver = webdriver.Chrome(service=service, options=options)
+#     # open the website
+#     driver.get("https://www.knowyourcountry.com/fatf-aml-list/")
+#     # wait for the page to load
+#     # waitTillLoaded(driver)
+#     return driver
+
+# def getList(driver,black):
+    # get the text from the following xpath when its loaded: /html/body/div[2]/main/div[3]/div/section/div/div/div/div/div/p[4]
+    # element = WebDriverWait(driver, 10).until()
+    # element = driver.find_element(By.XPATH, f"/html/body/div[2]/main/div[3]/div/section/div/div/div/div/div/p[{4 if black else 5}]")
+    # # get the text
+    # if black:
+    #     text = element.text.split("\n")[1:]
+    # else:
+    #     text = element.text.split("\n")[2:]
