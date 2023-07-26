6862c84c69d9c71fb5c12861a00645abe7bf72d3
allesman <allesman.stachelx@gmail.com>
2023-07-26 13:06:55 +0200

    changed verbose names and ordering for models

--- cpiScraper/models.py
+++ cpiScraper/models.py
@@ -3,14 +3,26 @@ from django.db import models
 class CPI(models.Model):
     iso3 = models.CharField(max_length=3,null=True)
     cpi_score = models.FloatField(null=True)
+    class Meta:
+        ordering = ['-cpi_score']
+        verbose_name_plural = "CPI"
 
 class FSI(models.Model):
     iso3 = models.CharField(max_length=3,null=True)
     fsi_score = models.FloatField(null=True)
+    class Meta:
+        ordering = ['-fsi_score']
+        verbose_name_plural = "FSI"
 
 class FATF(models.Model):
     iso3 = models.CharField(max_length=3,null=True)
     fatf_score = models.FloatField(max_length=100,null=True)
+    class Meta:
+        ordering = ['-fatf_score']
+        verbose_name_plural = "FATF"
 
 class Overview(models.Model):
     iso3 = models.CharField(max_length=3,null=True)
+    class Meta:
+        ordering = ['iso3']
+        verbose_name_plural = "Overview"
